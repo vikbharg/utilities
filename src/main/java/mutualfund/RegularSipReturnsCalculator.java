a3c4d40f9786f3531a7b2a4f255b1ce09c19f260
vikrant bhargava <vikrant.bhargava@amadeus.com>
2021-10-14 11:31:09 +0530

    Initial Commit

---
+++ src/main/java/mutualfund/RegularSipReturnsCalculator.java
@@ -0,0 +1,19 @@
+
+public class RegularSipReturnsCalculator {
+
+	public static void main(String[] args) {
+		
+		int sipAmount = 2000;
+		double rateOfInterest = 11;
+		int investmentDuration = 30;
+		
+		System.out.println(getReturns(sipAmount, rateOfInterest, investmentDuration));
+	}
+	
+	public static int getReturns(int sipAmount, double rateOfInterest, int investmentDuration) {
+		double i = rateOfInterest/12/100;
+		
+		Double corpusAmount =  sipAmount * (Math.pow(1+i, investmentDuration*12)-1) * ((1+i)/i);
+		return corpusAmount.intValue();
+	}
+}
---
+++ src/main/java/mutualfund/SteupSipReturnsCalculator.java
@@ -0,0 +1,38 @@
+
+public class SteupSipReturnsCalculator {
+
+	public static void main(String[] args) {
+
+		int sipAmount = 4000;
+		double rateOfInterest = 12;
+		int investmentDuration = 27;
+		double stepUpPercent = 10;
+		
+		System.out.println(getReturns(sipAmount, rateOfInterest, investmentDuration, stepUpPercent));
+	}
+	
+	public static int getReturns(int sipAmount, double rateOfInterest, int investmentDuration, double stepUpPercent) {
+		double i = rateOfInterest/12/100;
+		
+		Double corpusAmount =  sipAmount * (Math.pow(1+i, investmentDuration*12)-1) * ((1+i)/i);
+		Double previousSipAmount = Double.valueOf(sipAmount);
+		
+		long amountInvested = sipAmount * 12;
+		
+		for(int j=investmentDuration-1;j>=1;j--) {
+			
+			Double normalizedSipAmount = (sipAmount*Math.pow((1+stepUpPercent/100), investmentDuration-j));
+			
+			int effectiveSipAmount = normalizedSipAmount.intValue() - previousSipAmount.intValue();
+			previousSipAmount = normalizedSipAmount;
+			
+			Double extraCorpus = effectiveSipAmount * (Math.pow(1+i, j*12)-1) * ((1+i)/i);
+			corpusAmount = Double.sum(corpusAmount, extraCorpus);
+			amountInvested += normalizedSipAmount*12;
+			
+		}
+		System.out.println("Amount Invested : "+amountInvested);
+		
+		return corpusAmount.intValue();
+	}
+}
